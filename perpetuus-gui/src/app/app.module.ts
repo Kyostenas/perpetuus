9e770d12c6522ddac993cf241cfc4b2eae5e8bf4
Kyostenas <kyostenas@gmail.com>
2022-11-13 12:55:11 -0600

    feat: Aplicación con routing arranca.

--- perpetuus-gui/src/app/app.module.ts
+++ perpetuus-gui/src/app/app.module.ts
@@ -1,16 +1,41 @@
-import { NgModule } from '@angular/core';
+// (o-----------------------------------------( IMPORTACIONES ))
 import { BrowserModule } from '@angular/platform-browser';
 
+// FECHAS EN ESPAÑOL
+import { NgModule, LOCALE_ID } from '@angular/core';
+import localePy from '@angular/common/locales/es-MX';
+import {
+  registerLocaleData,
+  APP_BASE_HREF,
+  CommonModule,
+} from '@angular/common';
+
+// RUTAS
+import { APP_ROUTES } from './app.routes';
+
+// COMPONENTES
 import { AppComponent } from './app.component';
 
+// MODULOS
+import { HttpClientModule } from '@angular/common/http';
+import { JwtModule } from '@auth0/angular-jwt';
+
+registerLocaleData(localePy, 'es-MX');
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    CommonModule,
+    BrowserModule,
+    BrowserModule,
+    APP_ROUTES,
+    HttpClientModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    // Configuraciones de idioma.
+    { provide: APP_BASE_HREF, useValue: '/' },
+    { provide: LOCALE_ID, useValue: 'es-MX' },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ perpetuus-gui/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { RouterModule, Routes } from '@angular/router';
+
+
+const appRoutes: Routes = [
+
+];
+
+
+export const APP_ROUTES = RouterModule.forRoot(appRoutes, {useHash: false});
