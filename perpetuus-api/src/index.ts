60a83cb16dcc7684471f0c3b290e957123c058a1
Kyostenas <kyostenas@gmail.com>
2023-12-25 18:35:05 -0600

    chore: Reacomodo

--- perpetuus-api/src/index.ts
+++ perpetuus-api/src/index.ts
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-import dotenv from 'dotenv';  // Para archivos .env
-dotenv.config()
-
-// (o-----------------------------------------( IMPORTACIONES ))
-import express, { Application, Request, Response } from 'express';
-import mongoose from 'mongoose';
-mongoose.set('strictQuery', false)
-import { syslog as _syslog } from './utiles/logs';
-const syslog = _syslog(module)
-import { Resp } from './utiles/response';
-mongoose.Promise = global.Promise
-
-import { RUTA_ROL } from './componentes/usuario/rol-usuario/rol-usuario.routes';
-=======
 // (o-----------------------------------------( IMPORTACIONES ))
 import express, { Application, Request, Response } from 'express';
 import mongoose from 'mongoose';
@@ -24,17 +9,13 @@ mongoose.Promise = global.Promise;
 // import cors from 'cors';
 
 import { URI_DB, PORT } from './config';
->>>>>>> main
 
 // (o-----------------------------------------( CONFIGURACIONES ))
 
 const app: Application = express();
 
-<<<<<<< HEAD
-=======
 // app.use(cors());
 
->>>>>>> main
 // LOGS REQUESTS
 app.use((req, res, next) => {
     syslog.__request(req.method, req.url);
@@ -45,19 +26,6 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
-<<<<<<< HEAD
-// CONSTANTES ENV
-const PORT = process.env.PORT || 9000;
-const URI_DB = process.env.URI_DB
-
-// (o-----------------------------------------( RUTAS ))
-
-app.get('/', async (req: Request, res: Response): Promise<Response> => {
-  return new Resp(res, __filename, { mensaje: 'API Funcionando' })._200_ok()
-});
-
-app.use('/roles', RUTA_ROL());
-=======
 // (o-----------------------------------------( RUTAS ))
 
 app.all('*', (req, res, next)=>{
@@ -72,7 +40,6 @@ app.get('/api', async (req: Request, res: Response): Promise<Response> => {
 
 // app.use('/roles', RUTA_ROL());
 // app.use('/api/multi-consulta', RUTA_MULTI_CONSULTA())
->>>>>>> main
 
 // (o-----------------------------------------( CONECCION MONGODB ))
 
@@ -93,8 +60,4 @@ try {
   });
 } catch (error: any) {
   syslog.error( `Ocurrió un error antes de escuchar en el puerto: ${error.message}`)
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> main
