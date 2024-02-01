cee0910cd6a2e18f19753f3d131fe703f7d27948
Kyostenas <kyostenas@gmail.com>
2024-02-01 12:18:55 -0600

    chore: Componentes para formulario dinamico

---
+++ perpetuus-gui/src/app/components/utiles/formularios/formulario-dinamico-campo/formulario-dinamico-campo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario-dinamico-campo',
+  standalone: true,
+  imports: [],
+  templateUrl: './formulario-dinamico-campo.component.html',
+  styleUrl: './formulario-dinamico-campo.component.scss'
+})
+export class FormularioDinamicoCampoComponent {
+
+}
---
+++ perpetuus-gui/src/app/components/utiles/formularios/formulario-dinamico/formulario-dinamico.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario-dinamico',
+  standalone: true,
+  imports: [],
+  templateUrl: './formulario-dinamico.component.html',
+  styleUrl: './formulario-dinamico.component.scss'
+})
+export class FormularioDinamicoComponent {
+
+}
