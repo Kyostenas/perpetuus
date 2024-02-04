d21e012d2ce58dd716815aadc401ba97fc8f1125
Kyostenas <kyostenas@gmail.com>
2024-02-03 22:12:17 -0600

    chore: Partes visuales de signin y signup

--- perpetuus-gui/src/app/components/pages/inicio/signin/signin.component.ts
+++ perpetuus-gui/src/app/components/pages/inicio/signin/signin.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { Validators } from '@angular/forms';
+import { FormularioDinamicoComponent } from 'src/app/components/utiles/formularios/formulario-dinamico/formulario-dinamico.component';
+import { CardComponent } from 'src/app/components/utiles/varios/card/card.component';
+import { CampoBaseFormularioDinamico, CampoFormulario } from 'src/app/models/utiles/formularios/formulario-dinamico-campo-base.model';
 
 @Component({
   selector: 'app-signin',
   standalone: true,
-  imports: [],
+  imports: [
+    FormularioDinamicoComponent,
+    CardComponent,
+  ],
   templateUrl: './signin.component.html',
   styleUrl: './signin.component.scss'
 })
 export class SigninComponent {
+
+  datos_formulario: CampoBaseFormularioDinamico<string>[] = [
+    new CampoFormulario({
+      llave: 'nombre_usuario',
+      etiqueta: 'Usuario',
+      tipo: 'text',
+      clase_columna: 'col-12'
+    }),
+    new CampoFormulario({
+      llave: 'contrasena',
+      etiqueta: 'Contraseña',
+      tipo: 'password'
+    }),
+  ]
+
+  get datos_formulario_json_string(): string {
+    return JSON.stringify(this.datos_formulario, undefined, 2);
+  }
 
 }
--- perpetuus-gui/src/app/components/pages/inicio/signup/signup.component.ts
+++ perpetuus-gui/src/app/components/pages/inicio/signup/signup.component.ts
@@ -1,12 +1,82 @@
 import { Component } from '@angular/core';
+import { Validators } from '@angular/forms';
+import { FormularioDinamicoComponent } from 'src/app/components/utiles/formularios/formulario-dinamico/formulario-dinamico.component';
+import { CardComponent } from 'src/app/components/utiles/varios/card/card.component';
+import { CampoBaseFormularioDinamico, CampoFormulario } from 'src/app/models/utiles/formularios/formulario-dinamico-campo-base.model';
+import { REGEX_VALIDACION_CORREO } from 'src/app/services/utiles/varios/utilidades/utilidades.service';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [
+    FormularioDinamicoComponent,
+    CardComponent,
+  ],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
+
+  datos_formulario: CampoBaseFormularioDinamico<string>[] = [
+    new CampoFormulario({
+      llave: 'nombres',
+      etiqueta: 'Nombres',
+      tipo: 'text',
+      clase_columna: 'col-12',
+      validaciones_campo: [
+        Validators.required,
+        Validators.minLength(3),
+      ]
+    }),
+    new CampoFormulario({
+      llave: 'apellidos',
+      etiqueta: 'Apellidos',
+      tipo: 'text',
+      clase_columna: 'col-12',
+      validaciones_campo: [
+        Validators.required,
+        Validators.minLength(3),
+      ]
+    }),
+    new CampoFormulario({
+      llave: 'nombre_usuario',
+      etiqueta: 'Usuario',
+      tipo: 'text',
+      clase_columna: 'col-12',
+      validaciones_campo: [
+        Validators.required,
+        Validators.minLength(3),
+      ]
+    }),
+    new CampoFormulario({
+      llave: 'correo',
+      etiqueta: 'Correo',
+      tipo: 'email',
+      validaciones_campo: [
+        Validators.pattern(REGEX_VALIDACION_CORREO)
+      ]
+    }),
+    new CampoFormulario({
+      llave: 'contrasena',
+      etiqueta: 'Contraseña (de 5 a 25 caracteres)',
+      tipo: 'password',
+      validaciones_campo: [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(25),
+      ]
+    }),
+    new CampoFormulario({
+      llave: 'numero_celular',
+      etiqueta: 'Número Celular',
+      tipo: 'tel',
+      validaciones_campo: [
+        Validators.required,
+        Validators.minLength(10),
+        Validators.maxLength(10),
+      ]
+    }),
+
+  ]
 
 }
