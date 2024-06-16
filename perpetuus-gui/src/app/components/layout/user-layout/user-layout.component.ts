4a39c05a0d4cbd343878cc6a4653c20d5c1d9103
Kyostenas <kyostenas@gmail.com>
2024-06-16 17:58:32 -0600

    feat(gui): Validaciones de sesion en layouts de admin y usuario

--- perpetuus-gui/src/app/components/layout/user-layout/user-layout.component.ts
+++ perpetuus-gui/src/app/components/layout/user-layout/user-layout.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/inicio/signin/auth.service';
 
 @Component({
   selector: 'app-user-layout',
@@ -7,6 +9,20 @@ import { Component } from '@angular/core';
   templateUrl: './user-layout.component.html',
   styleUrl: './user-layout.component.scss'
 })
-export class UserLayoutComponent {
+export class UserLayoutComponent implements OnInit {
+
+  constructor(
+    private auth_service: AuthService,
+    private router: Router,
+  ) { }
+
+  ngOnInit(): void {
+    this.auth_service.validar_sesion()
+    .subscribe(sesion_es_valida => {
+      if (!sesion_es_valida) {
+        this.router.navigate(['inicio/signin'])
+      }
+    }, err => this.router.navigate(['inicio/signin']))    
+  }
 
 }
