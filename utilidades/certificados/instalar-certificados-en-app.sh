3c598e0255d587831f2e051da1913e72818869b6
Kyostenas <kyostenas@gmail.com>
2024-10-13 03:11:24 -0600

    refactor(utilidades): error en las scripts de certificacion

--- utilidades/certificados/instalar-certificados-en-app.sh
+++ utilidades/certificados/instalar-certificados-en-app.sh
@@ -2,25 +2,30 @@
 
 # Llamar el comando de certbot en el contenedor (ya debe haberse
 # ejeutado el docker compose up)
+    # --webroot 
+        # Usar el plugin de webroot para que en ese lugar se 
+        # guarden los certificados
+    # --webroot-path=/var/www/html 
+        # Definir el directorio que se considera el webroot
+    # --email kyostenas@gmail.com 
+        # Especificar un email para el registro y recuperacion de los
+        # certificados. 
+    # --no-eff-email 
+        # No se quiere compartir el email con la Electronic Frontier
+        # Foundation (EFF)
+    # --agree-tos 
+        # Estar de acuerdo con el acuerdo de subscripcion de ACME
+    # --force-renewal 
+        # Especificar el dominio que se quiere certificar
+    # -d perpetuus.mx
+        # Especificar el dominio que se quiere certificar
 docker-exec -t app_perpetuus certbot certonly \
-    # Usar el plugin de webroot para que en ese lugar se 
-    # guarden los certificados
     --webroot \
-    # Definir el directorio que se considera el webroot
     --webroot-path=/var/www/html \
-    # Especificar un email para el registro y recuperacion de los
-    # certificados. 
     --email kyostenas@gmail.com \
-    # No se quiere compartir el email con la Electronic Frontier
-    # Foundation (EFF)
     --no-eff-email \
-    # Estar de acuerdo con el acuerdo de subscripcion de ACME
     --agree-tos \
-    # Indicarle a certbot que se quiere solicitar un nuevo certificado
-    # con el mismo dominio en caso de que ya exista uno (generado
-    # previamente)
     --force-renewal \
-    # Especificar el dominio que se quiere certificar
     -d perpetuus.mx
 
 echo "==> certificados generados"
--- utilidades/certificados/prueba-certificados-en-app.sh
+++ utilidades/certificados/prueba-certificados-en-app.sh
@@ -1,28 +1,37 @@
 #!/bin/bash
 
+# Esta es una script de prueba para ver se es posible hacer la
+# solicitud de certificacion de letsencrypt con certbod.
 # Llamar el comando de certbot en el contenedor (ya debe haberse
 # ejeutado el docker compose up)
+    # --webroot 
+        # Usar el plugin de webroot para que en ese lugar se 
+        # guarden los certificados
+    # --webroot-path=/var/www/html 
+        # Definir el directorio que se considera el webroot
+    # --email kyostenas@gmail.com 
+        # Especificar un email para el registro y recuperacion de los
+        # certificados. 
+    # --no-eff-email 
+        # No se quiere compartir el email con la Electronic Frontier
+        # Foundation (EFF)
+    # --agree-tos 
+        # Estar de acuerdo con el acuerdo de subscripcion de ACME
+    # --staging 
+        # Esto es para indicarle a certbod que se quiere usar el
+        # entorno de staging de Let's Encrypt al obtener los certificados.
+        # Esto permite que se prueben las configuraciones establecidas
+        # y se eviten los limites de solicitud de dominio (hay un limite
+        # de 7 solicitudes cada 20 dias)
+    # -d perpetuus.mx
+        # Especificar el dominio que se quiere certificar
 docker-exec -t app_perpetuus certbot certonly \
-    # Usar el plugin de webroot para que en ese lugar se 
-    # guarden los certificados
     --webroot \
-    # Definir el directorio que se considera el webroot
     --webroot-path=/var/www/html \
-    # Especificar un email para el registro y recuperacion de los
-    # certificados. 
     --email kyostenas@gmail.com \
-    # No se quiere compartir el email con la Electronic Frontier
-    # Foundation (EFF)
     --no-eff-email \
-    # Estar de acuerdo con el acuerdo de subscripcion de ACME
     --agree-tos \
-    # Esto es para indicarle a certbod que se quiere usar el
-    # entorno de staging de Let's Encrypt al obtener los certificados.
-    # Esto permite que se prueben las configuraciones establecidas
-    # y se eviten los limites de solicitud de dominio (hay un limite
-    # de 7 solicitudes cada 20 dias)
     --staging \
-    # Especificar el dominio que se quiere certificar
     -d perpetuus.mx
 
 # Mostar los certificados obtenidos para confirmar visualmente que
