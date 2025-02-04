a3cff2192623f93fe3bc01c3fafda9a2f1fea6ce
Kyostenas <kyostenas@gmail.com>
2025-02-04 02:56:21 -0600

    feat: componente de paginacion

---
+++ perpetuus-gui/src/app/components/utiles/varios/paginador-generico/paginador-generico.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-paginador-generico',
+  imports: [],
+  templateUrl: './paginador-generico.component.html',
+  styleUrl: './paginador-generico.component.scss'
+})
+export class PaginadorGenericoComponent {
+
+}
