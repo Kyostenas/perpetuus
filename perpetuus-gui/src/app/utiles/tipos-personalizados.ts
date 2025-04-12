675c286c0e8f01d9334a972a9cdee33fe1bbe244
Kyostenas <kyostenas@gmail.com>
2025-04-11 19:19:16 -0600

    chore(frontend): dynamic type for deep keys
    it will take the keys (even nested) of an object and concatenate them in a union type

--- perpetuus-gui/src/app/utiles/tipos-personalizados.ts
+++ perpetuus-gui/src/app/utiles/tipos-personalizados.ts
@@ -35,6 +35,49 @@ export type ValoresAnidadosRecursivosDeObjeto<T, tipo_valor> = T extends object
         : never
     : never;
 
+/**
+ * Este es un tipo dinamico que permite extrar todas las llaves
+ * (incluso anidadas) de un objeto y convertirlos en un solo
+ * tipo.
+ * ```
+ * const OBJETO = {
+ *      a: 'valor1',
+ *      b: 'valor2',
+ *      c: {
+ *          d: 'valor3'
+ *      },
+ * } as const;
+ * ```
+ *
+ * Y si se quiere obtener un tipo de todas sus llaves, se hace
+ * esto:
+ * ```
+ * export type TIPOS_OBJETO =
+ *      ValoreAnidadosRecursivosDeObjeto<typeof OBJETO>
+ * ```
+ *
+ * Que es equivalente a hacer:
+ * ```
+ * export type TIPOS_OBJETO = 'a' | 'b' | 'c.d'
+ * ```
+ *
+ * La diferencia es que esto funciona dinamicamente y el tipado
+ * es seguro, por lo que si permite el autocompletado.
+ */
+export type DeepKeys<OBJECT> = OBJECT extends any[] 
+    ? never
+    : OBJECT extends object
+    ? {
+        [KEY in keyof OBJECT]: KEY extends string
+            ? OBJECT[KEY] extends (...args: any[]) => any
+                ? never
+                : Exclude<OBJECT[KEY], undefined> extends object
+                ? `${KEY}` | `${KEY}.${DeepKeys<Exclude<OBJECT[KEY], undefined>>}`
+                : `${KEY}`
+            : never;
+    }[keyof OBJECT]
+    : never;
+
 export type Paginacion = {
     limite: number;
     desde: number;
