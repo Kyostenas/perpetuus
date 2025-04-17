e8f5c22a843b06f39279e7b9ebcaded89ce5f2f4
Kyostenas <kyostenas@gmail.com>
2025-04-16 22:50:41 -0600

    feat(frontend): new query to open form edition

--- perpetuus-gui/src/app/services/utiles/estructurales/control-queries-url/control-queries-url.service.ts
+++ perpetuus-gui/src/app/services/utiles/estructurales/control-queries-url/control-queries-url.service.ts
@@ -1,8 +1,12 @@
-import { computed, effect, Injectable, Signal } from '@angular/core';
+import { computed, effect, Injectable, OnInit, Signal } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Paginacion } from 'src/app/utiles/tipos-personalizados';
+import { Pagination } from 'src/app/utiles/tipos-personalizados';
 import { toSignal } from '@angular/core/rxjs-interop';
 
+/**
+ * This service contains all the allowed query params that can be used
+ * on the frontend url.
+ */
 @Injectable({
   providedIn: 'root'
 })
@@ -15,30 +19,38 @@ export class ControlQueriesUrlService {
     this.query_string = toSignal(this.route.queryParams, {initialValue: {}}) as Signal<QUERY_PARAMS_GENERAL | undefined>
     effect(() => {
       // console.log(this.query_string())
-      this.query_actual = computed(() => {
-        try {
-          let objeto_final: {[type: string]: any} = {}
-          for (let [nombre, valor_string] of Object.entries(
-            this.query_string() as QUERY_PARAMS_GENERAL
-          )) {
-            objeto_final[nombre] = JSON.parse(valor_string)
-          }
-          return objeto_final as QUERY_PARAMS_GENERAL
-        } catch {
-          return {} as QUERY_PARAMS_GENERAL;
+      this.query_actual = this.get_computed_value()
+    })
+  }
+
+  private get_computed_value() {
+    return computed(() => {
+      try {
+        let objeto_final: {[type: string]: any} = {}
+        for (let [nombre, valor_string] of Object.entries(
+          this.query_string() as QUERY_PARAMS_GENERAL
+        )) {
+          objeto_final[nombre] = JSON.parse(valor_string)
         }
-      })
+        return objeto_final as QUERY_PARAMS_GENERAL
+      } catch {
+        return {} as QUERY_PARAMS_GENERAL;
+      }
     })
   }
 
   private query_string!: Signal<QUERY_PARAMS_GENERAL | undefined>
-  query_actual!: Signal<QUERY_PARAMS_GENERAL> 
+  query_actual: Signal<QUERY_PARAMS_GENERAL> = this.get_computed_value()
 
+  /**
+   * The collection of all the avialable queries
+   */
   queries = {
-    paginacion: this.preparar_query<Paginacion>('paginacion'),
-    busqueda_global: this.preparar_query<{valor: string}>('busqueda_global'),
-    busqueda_por_termino: this.preparar_query<{valor: string}>('busqueda_por_termino'),
-    filtros: <T>() => this.preparar_query<T>('filtros'),
+    pagination: this.preparar_query<Pagination>('pagination'),
+    global_search: this.preparar_query<{valor: string}>('global_search'),
+    term_search: this.preparar_query<{valor: string}>('term_search'),
+    filters: <T>() => this.preparar_query<T>('filters'),
+    editing_form: this.preparar_query<boolean>('editing_form')
   }
 
   private accion<T>(nombre: string) {
@@ -80,8 +92,11 @@ export class ControlQueriesUrlService {
 }
 
 export interface QUERY_PARAMS_GENERAL {
-  paginacion: Paginacion,
-  filtros: any
+  pagination: Pagination,
+  filters: any
+  global_search: string,
+  term_search: string,
+  editing_form: boolean,
 }
 
 
